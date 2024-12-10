12560e441fa90f0245527fdd5340483cde4697b0
CHETTIM CHETTY HEMASRI <131608043+HEMASRI2175@users.noreply.github.com>
2024-12-10 17:39:52 +0530

    Update facebook_comments.py

--- facebook_comments.py
+++ facebook_comments.py
@@ -4,7 +4,6 @@ import streamlit as st
 
 # Load environment variables
 load_dotenv()
-
 acebook_api_version = "v21.0"  
 facebook_access_token = "EAAP2jbgg3VIBOzaOsZCbFZACZA7vwhruVVAYzre10y0BU1vuIHd13lzuuTKFDFJqs3DLnSz0ZAjuyNyC0jEaIaSOgClgo9J3DRTjZCiGEvDhHcgaZC44k5O3Vuw1BRskZBlyJaEWMD5de1ErJEw99zTsSrseb2B1ZAN9UqtJAdCOZCKxdIp2Bc9T7zslWshjTaihZAbzSIlpnnS56Hklyqk7zgwHHle53qSoonYGgZD"
 def start_facebook_service():
